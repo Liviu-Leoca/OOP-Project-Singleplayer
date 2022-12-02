8a58ab63c3837a8aa9951b6ec6ecca27ea8539a3
leocaliviunicolae@gmail.com <1119419056+Liviu-Leoca@users.noreply.github.com>
2022-12-02 22:20:56 +0200

    Added classes

--- Ticketing App/Ticketing app.cpp
+++ Ticketing App/Ticketing app.cpp
@@ -1,5 +1,42 @@
 #include<iostream>
 
+using namespace std;
+class location {
+private:
+	int* maxSeats;
+	int rows;
+	string zone;
+	const int CAENCode;
+private:
+	static string CINEMAname;
+};
+
+class event
+{
+	friend class ticket;
+private:
+	char* movieName;
+	int timeStart;
+
+
+	int normal, normalPrice;
+	int vip, vipPrice;
+	int d, m, y;
+};
+
+class ticket
+{
+private:
+	int ticketID;
+	event movieName;
+	event timeStart;
+	char* client;
+	int age;
+	string normalOrVip;
+	int noSeats;
+	int d, m, y;
+};
+
 int main()
 {
 	std::cout << std::endl << "Hello World!";
